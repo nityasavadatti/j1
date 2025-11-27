4bd72f341c6e72ac1c9bd68c9495a41bc49cf3b1
nityasavadatti <nityasavadatti39@gmail.com>
2025-11-27 16:55:27 +0530

    save

---
+++ journal5.py
@@ -0,0 +1,22 @@
+import sys
+
+if len(sys.argv) == 4:
+    principal = float(sys.argv[1])
+    rate = float(sys.argv[2])
+    time = float(sys.argv[3])
+else:
+    print("No command-line input given — using input() method:")
+    principal = float(input("Enter principal: "))
+    rate = float(input("Enter rate: "))
+    time = float(input("Enter time: "))
+
+# Validate values
+if principal <= 0:
+    print("Principal must be greater than 0")
+elif rate <= 0:
+    print("Rate must be greater than 0")
+elif time <= 0:
+    print("Time must be greater than 0")
+else:
+    simple_interest = (principal * rate * time) / 100
+    print("Simple Interest =", simple_interest)
